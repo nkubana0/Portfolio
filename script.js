5b0a75176989bcfe95c334d37442794a8b5b2905
nkubana0 <i.shema@alustudent.com>
2024-02-20 17:38:28 +0200

    added dark mode

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get all navigation links
+    
     const links = document.querySelectorAll('.navigation a');
-
-    // Loop through each link
+    
     links.forEach(link => {
-        // Create a line element
+        
         const line = document.createElement('div');
+        
         line.classList.add('underline');
-
-        // Append the line to the link
+        
         link.appendChild(line);
-
-        // Add event listener for mouseenter
+        
         link.addEventListener('mouseenter', () => {
-            // Animate the width of the line to match the width of the link's text content
+            
             line.style.width = `${link.offsetWidth}px`;
-            line.style.opacity = 1; // Show the line
+            
+            line.style.opacity = 1;
         });
-
-        // Add event listener for mouseleave
+        
         link.addEventListener('mouseleave', () => {
-            // Reset the width of the line
+            
             line.style.width = '0';
-            line.style.opacity = 0; // Hide the line
+            line.style.opacity = 0;
         });
     });
+    
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const modeIcon = document.getElementById('modeIcon');
+    
+    darkModeToggle.addEventListener('click', function() {
+        
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            modeIcon.src = 'Images/light.svg';
+            modeIcon.alt = 'Light Mode Icon';
+        } else {
+            modeIcon.src = 'Images/dark.svg';
+            modeIcon.alt = 'Dark Mode Icon';
+    }
+});
 });
