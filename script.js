db2a3e4fe03cdaa2054c808ac6275c2118eeb679
nkubana0 <i.shema@alustudent.com>
2024-03-17 17:26:27 +0200

    my portfolio

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".navigation a");
-
-  links.forEach((link) => {
-    const line = document.createElement("div");
-
-    line.classList.add("underline");
-
-    link.appendChild(line);
-
-    link.addEventListener("mouseenter", () => {
-      line.style.width = `${link.offsetWidth}px`;
-
-      line.style.opacity = 1;
-    });
-
-    link.addEventListener("mouseleave", () => {
-      line.style.width = "0";
-      line.style.opacity = 0;
-    });
-  });
-
-  const darkModeToggle = document.getElementById("darkModeToggle");
-  const modeIcon = document.getElementById("modeIcon");
-
-  darkModeToggle.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-    if (document.body.classList.contains("dark-mode")) {
-      modeIcon.src = "Images/light.svg";
-      modeIcon.alt = "Light Mode Icon";
-    } else {
-      modeIcon.src = "Images/dark.svg";
-      modeIcon.alt = "Dark Mode Icon";
-    }
-  });
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open")
+  icon.classList.toggle("open")
+}
